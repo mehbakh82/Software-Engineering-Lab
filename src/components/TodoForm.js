bbbfdba7113be9a967a9b5495cde54e5d8d2a069
Mehran Bakhtiari <mehbakh82@gmail.com>
2025-08-08 17:39:56 +0330

    feat: Implement functionality to add new todos

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,10 +1,23 @@
-// src/components/TodoForm.js
-import React from 'react';
+import React, { useState } from 'react';
 
-function TodoForm() {
+function TodoForm({ addTodo }) {
+  const [value, setValue] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!value) return;
+    addTodo(value);
+    setValue('');
+  };
+
   return (
-    <form>
-      <input type="text" placeholder="Add a new to-do..." />
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+        placeholder="Add a new to-do..."
+      />
       <button type="submit">Add</button>
     </form>
   );
