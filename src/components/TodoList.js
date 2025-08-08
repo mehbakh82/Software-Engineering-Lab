365dbd2f402c91c11ac03a2a2295a06693a7927c
Mehran Bakhtiari <mehbakh82@Mehrans-MacBook-Pro.local>
2025-08-08 17:34:04 +0330

    feat(TodoList): Implement basic list component UI

---
+++ src/components/TodoList.js
@@ -0,0 +1,13 @@
+// src/components/TodoList.js
+import React from 'react';
+
+function TodoList() {
+  // We'll add the actual list later
+  return (
+    <div className="todo-list">
+      <p>Your tasks will appear here.</p>
+    </div>
+  );
+}
+
+export default TodoList;
