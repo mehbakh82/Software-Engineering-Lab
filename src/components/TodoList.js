68dc357b5eddc8a0bc62774bff2f8dc9ff9a44f6
Mehran Bakhtiari <mehbakh82@gmail.com>
2025-08-08 17:36:54 +0330

    feat(TodoList): Render list of todos from props

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,11 +1,16 @@
 // src/components/TodoList.js
 import React from 'react';
 
-function TodoList() {
-  // We'll add the actual list later
+function TodoList({ todos }) { // Destructure todos from props
   return (
     <div className="todo-list">
-      <p>Your tasks will appear here.</p>
+      <ul>
+        {todos.map(todo => (
+          <li key={todo.id} className={todo.completed ? 'completed' : ''}>
+            {todo.text}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
