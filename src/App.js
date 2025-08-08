c897318312e7e5c0f2431e8006dddd74d2a77134
Mehran Bakhtiari <mehbakh82@gmail.com>
2025-08-08 18:16:19 +0330

    fix: Resolve merge conflict in App.js header

--- src/App.js
+++ src/App.js
@@ -33,7 +33,7 @@ function App() {
   return (
     <div className="App">
       <header>
-        <h1>Simple Task Manager</h1>
+        <h1>Ultimate To-Do Tracker</h1>
       </header>
       <TodoForm addTodo={addTodo} />
       <TodoList todos={todos} deleteTodo={deleteTodo} toggleComplete={toggleComplete} />
